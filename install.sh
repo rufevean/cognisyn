6522f2ca8856ad696433bcc59f1744cb217448b8
Dheeraj Chowdary <95273312+rufevean@users.noreply.github.com>
2023-08-21 18:41:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Specify your tool name and directory name
+<<<<<<< HEAD
+TOOL_NAME="cognisyn"
+TOOL_DIR_NAME="cognisyn"
+=======
+>>>>>>> 4610987b7014f8f324dbf010cdf7ffc2a66855f2
 
 main() {
 	BIN_DIR=${BIN_DIR-"$HOME/.bin"} # Change the directory name if desired
@@ -46,12 +51,25 @@ main() {
 	ARCHITECTURE="$(uname -m)"
 	# Your custom architecture handling here if needed
 
+	if [ "${ARCHITECTURE}" = "x86_64" ]; then
+		# Redirect stderr to /dev/null to avoid printing errors if non Rosetta.
+		if [ "$(sysctl -n sysctl.proc_translated 2>/dev/null)" = "1" ]; then
+			ARCHITECTURE="aarch64" # Rosetta.
+		else
+			ARCHITECTURE="x86_64" # Intel.
+		fi
+	elif [ "${ARCHITECTURE}" = "arm64" ] || [ "${ARCHITECTURE}" = "aarch64" ]; then
+		ARCHITECTURE="aarch64" # Arm.
+	else
+		ARCHITECTURE="x86_64" # Amd.
+	fi
+
 	# Construct the URL for downloading the release archive from GitHub
 	BINARY_URL="https://github.com/rufevean/cognisyn/releases/latest/download/cognisyn-${PLATFORM}-${ARCHITECTURE}.tar.gz"
 	echo "$BINARY_URL"
 
 	echo "downloading latest binary"
-	ensure curl -L "$BINARY_URL" | tar xz -C "$BIN_DIR" "$TOOL_DIR_NAME/$TOOL_NAME"
+	ensure curl -L "$BINARY_URL" -o "$BIN_DIR/cognisyn"
 	chmod +x "$BIN_DIR/$TOOL_NAME"
 
 	echo "installed - $("$BIN_DIR/$TOOL_NAME" --version)"
