3301fc1a00d8284873df5676104f815ee368fcff
rufevean <deerajmydearalways@gmail.com>
2023-08-21 16:46:44 +0530

    Stage 1 - checking prefix

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+# Specify your tool name and directory name
+TOOL_NAME="cognisyn"
+TOOL_DIR_NAME="cogni"
+
+main() {
+	BIN_DIR=${BIN_DIR-"$HOME/.bin"} # Change the directory name if desired
+	mkdir -p "$BIN_DIR"
+
+	case $SHELL in
+	*/zsh)
+		PROFILE=$HOME/.zshrc
+		;;
+	*/bash)
+		PROFILE=$HOME/.bashrc
+		;;
+	*/fish)
+		PROFILE=$HOME/.config/fish/config.fish
+		;;
+	*/ash)
+		PROFILE=$HOME/.profile
+		;;
+	*)
+		echo "could not detect shell, manually add ${BIN_DIR} to your PATH."
+		exit 1
+		;;
+	esac
+
+	if [[ ":$PATH:" != *":${BIN_DIR}:"* ]]; then
+		echo >>"$PROFILE" && echo "export PATH=\"\$PATH:$BIN_DIR\"" >>"$PROFILE"
+	fi
+
+	PLATFORM="$(uname -s)"
+	case $PLATFORM in
+	Linux)
+		PLATFORM="linux"
+		;;
+	Darwin)
+		PLATFORM="darwin"
+		;;
+	*)
+		err "unsupported platform: $PLATFORM"
+		;;
+	esac
+
+	ARCHITECTURE="$(uname -m)"
+	# Your custom architecture handling here if needed
+
+	# Construct the URL for downloading the release archive from GitHub
+	BINARY_URL="https://github.com/rufevean/cognisyn/releases/latest/download/${TOOL_NAME}-${PLATFORM}-${ARCHITECTURE}.tar.gz"
+	echo "$BINARY_URL"
+
+	echo "downloading latest binary"
+	ensure curl -L "$BINARY_URL" | tar xz -C "$BIN_DIR" "$TOOL_DIR_NAME/$TOOL_NAME"
+	chmod +x "$BIN_DIR/$TOOL_NAME"
+
+	echo "installed - $("$BIN_DIR/$TOOL_NAME" --version)"
+}
+
+# Run a command that should never fail.
+ensure() {
+	if ! "$@"; then err "command failed: $*"; fi
+}
+
+main "$@" || exit 1
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,144 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+use bat::PrettyPrinter;
+use clap::Parser;
+use colored::Colorize;
+use question::{Answer, Question};
+use reqwest::blocking::Client;
+use serde_json::json;
+use spinners::{Spinner, Spinners};
+use std::process::Command;
+
+mod config;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Description of the command to execute
+    prompt: Vec<String>,
+
+    /// Run the generated program without asking for confirmation
+    #[clap(short = 'y', long)]
+    force: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let config = config::Config::new();
+    let client = Client::new();
+    let mut spinner = Spinner::new(Spinners::BouncingBar, "Generating your command...".into());
+    let api_addr = format!("{}/completions", config.api_base);
+    let response = client
+        .post(api_addr)
+        .json(&json!({
+            "top_p": 1,
+            "stop": "```",
+            "temperature": 0,
+            "suffix": "\n```",
+            "max_tokens": 1000,
+            "presence_penalty": 0,
+            "frequency_penalty": 0,
+            "model": "text-davinci-003",
+            "prompt": build_prompt(&cli.prompt.join(" ")),
+        }))
+        .header("Authorization", format!("Bearer {}", config.api_key))
+        .send()
+        .unwrap();
+
+    let status_code = response.status();
+    if status_code.is_client_error() {
+        let response_body = response.json::<serde_json::Value>().unwrap();
+        let error_message = response_body["error"]["message"].as_str().unwrap();
+        spinner.stop_and_persist(
+            "✖".red().to_string().as_str(),
+            format!("API error: \"{error_message}\"").red().to_string(),
+        );
+        std::process::exit(1);
+    } else if status_code.is_server_error() {
+        spinner.stop_and_persist(
+            "✖".red().to_string().as_str(),
+            format!("OpenAI is currently experiencing problems. Status code: {status_code}")
+                .red()
+                .to_string(),
+        );
+        std::process::exit(1);
+    }
+
+    let code = response.json::<serde_json::Value>().unwrap()["choices"][0]["text"]
+        .as_str()
+        .unwrap()
+        .trim()
+        .to_string();
+
+    spinner.stop_and_persist(
+        "✔".green().to_string().as_str(),
+        "Got some code!".green().to_string(),
+    );
+
+    PrettyPrinter::new()
+        .input_from_bytes(code.as_bytes())
+        .language("bash")
+        .grid(true)
+        .print()
+        .unwrap();
+
+    let should_run = if cli.force {
+        true
+    } else {
+        Question::new(
+            ">> Run the generated program? [Y/n]"
+                .bright_black()
+                .to_string()
+                .as_str(),
+        )
+        .yes_no()
+        .until_acceptable()
+        .default(Answer::YES)
+        .ask()
+        .expect("Couldn't ask question.")
+            == Answer::YES
+    };
+
+    if should_run {
+        spinner = Spinner::new(Spinners::BouncingBar, "Executing...".into());
+
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(code.as_str())
+            .output()
+            .unwrap_or_else(|_| {
+                spinner.stop_and_persist(
+                    "✖".red().to_string().as_str(),
+                    "Failed to execute the generated program.".red().to_string(),
+                );
+                std::process::exit(1);
+            });
+
+        if !output.status.success() {
+            spinner.stop_and_persist(
+                "✖".red().to_string().as_str(),
+                "The program threw an error.".red().to_string(),
+            );
+            println!("{}", String::from_utf8_lossy(&output.stderr));
+            std::process::exit(1);
+        }
+
+        spinner.stop_and_persist(
+            "✔".green().to_string().as_str(),
+            "Command ran successfully".green().to_string(),
+        );
+
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    }
+}
+
+fn build_prompt(prompt: &str) -> String {
+    let os_hint = if cfg!(target_os = "macos") {
+        " (on macOS)"
+    } else if cfg!(target_os = "linux") {
+        " (on Linux)"
+    } else {
+        ""
+    };
+
+    format!("{prompt}{os_hint}:\n```bash\n#!/bin/bash\n")
 }
