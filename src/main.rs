1773bd7c4a6f68fd1891c0880ed126483e9567b2
rufevean <deerajmydearalways@gmail.com>
2023-08-26 16:38:35 +0530

    stage 1 - prompt done

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-use bat::PrettyPrinter;
 use clap::Parser;
-use colored::Colorize;
-use question::{Answer, Question};
-use reqwest::blocking::Client;
+use reqwest::Client;
 use serde_json::json;
-use spinners::{Spinner, Spinners};
-use std::process::Command;
-
-mod config;
+use serde_json::Value;
+use std::{env, process::exit};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Description of the command to execute
+struct Args {
+    #[clap(short, long)]
     prompt: Vec<String>,
-
-    /// Run the generated program without asking for confirmation
-    #[clap(short = 'y', long)]
-    force: bool,
 }
 
-fn main() {
-    let cli = Cli::parse();
-    let config = config::Config::new();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = env::var("OPENAI_API_KEY").unwrap_or_else(|_| {
+        eprintln!("API_KEY is not set");
+        exit(1)
+    });
+
+    let args = Args::parse();
+    let prompt = args.prompt;
     let client = Client::new();
-    let mut spinner = Spinner::new(Spinners::BouncingBar, "Generating your command...".into());
-    let api_addr = format!("{}/completions", config.api_base);
+    let api_base =
+        env::var("OPENAI_API_BASE").unwrap_or_else(|_| String::from("https://api.openai.com/v1"));
+
+    let api_addr = format!("{}/completions", api_base);
+
     let response = client
         .post(api_addr)
         .json(&json!({
@@ -38,107 +37,30 @@ fn main() {
             "presence_penalty": 0,
             "frequency_penalty": 0,
             "model": "text-davinci-003",
-            "prompt": build_prompt(&cli.prompt.join(" ")),
+            "prompt": build_prompt(&prompt.join(" ")),
         }))
-        .header("Authorization", format!("Bearer {}", config.api_key))
+        .header("Authorization", format!("Bearer {}", api_key))
         .send()
+        .await
         .unwrap();
 
-    let status_code = response.status();
-    if status_code.is_client_error() {
-        let response_body = response.json::<serde_json::Value>().unwrap();
-        let error_message = response_body["error"]["message"].as_str().unwrap();
-        spinner.stop_and_persist(
-            "✖".red().to_string().as_str(),
-            format!("API error: \"{error_message}\"").red().to_string(),
-        );
-        std::process::exit(1);
-    } else if status_code.is_server_error() {
-        spinner.stop_and_persist(
-            "✖".red().to_string().as_str(),
-            format!("OpenAI is currently experiencing problems. Status code: {status_code}")
-                .red()
-                .to_string(),
-        );
-        std::process::exit(1);
-    }
+    // Extract and print the status code
+    let response_json: Value = response.json().await?;
 
-    let code = response.json::<serde_json::Value>().unwrap()["choices"][0]["text"]
-        .as_str()
-        .unwrap()
-        .trim()
-        .to_string();
-
-    spinner.stop_and_persist(
-        "✔".green().to_string().as_str(),
-        "Got some code!".green().to_string(),
-    );
-
-    PrettyPrinter::new()
-        .input_from_bytes(code.as_bytes())
-        .language("bash")
-        .grid(true)
-        .print()
-        .unwrap();
-
-    let should_run = if cli.force {
-        true
-    } else {
-        Question::new(
-            ">> Run the generated program? [Y/n]"
-                .bright_black()
-                .to_string()
-                .as_str(),
-        )
-        .yes_no()
-        .until_acceptable()
-        .default(Answer::YES)
-        .ask()
-        .expect("Couldn't ask question.")
-            == Answer::YES
-    };
-
-    if should_run {
-        spinner = Spinner::new(Spinners::BouncingBar, "Executing...".into());
-
-        let output = Command::new("bash")
-            .arg("-c")
-            .arg(code.as_str())
-            .output()
-            .unwrap_or_else(|_| {
-                spinner.stop_and_persist(
-                    "✖".red().to_string().as_str(),
-                    "Failed to execute the generated program.".red().to_string(),
-                );
-                std::process::exit(1);
-            });
-
-        if !output.status.success() {
-            spinner.stop_and_persist(
-                "✖".red().to_string().as_str(),
-                "The program threw an error.".red().to_string(),
-            );
-            println!("{}", String::from_utf8_lossy(&output.stderr));
-            std::process::exit(1);
+    // Extract the text from the JSON
+    if let Some(choices) = response_json
+        .get("choices")
+        .and_then(|choices| choices.as_array())
+    {
+        if let Some(choice) = choices.first() {
+            if let Some(text) = choice.get("text").and_then(|text| text.as_str()) {
+                println!("{}", text);
+            }
         }
-
-        spinner.stop_and_persist(
-            "✔".green().to_string().as_str(),
-            "Command ran successfully".green().to_string(),
-        );
-
-        println!("{}", String::from_utf8_lossy(&output.stdout));
     }
+    Ok(())
 }
 
 fn build_prompt(prompt: &str) -> String {
-    let os_hint = if cfg!(target_os = "macos") {
-        " (on macOS)"
-    } else if cfg!(target_os = "linux") {
-        " (on Linux)"
-    } else {
-        ""
-    };
-
-    format!("{prompt}{os_hint}:\n```bash\n#!/bin/bash\n")
+    format!("{prompt}:\n```bash\n#!/bin/bash\n", prompt = prompt)
 }
